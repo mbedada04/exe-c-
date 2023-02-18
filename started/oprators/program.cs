a18bab6750879fba319f5041bab6d571ff27d5cf
mbedada04 <mbedada@atl.yearup.org>
2023-02-17 19:12:38 -0500

    cSharp

---
+++ started/oprators/program.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace VarsAndData
+{
+class Program
+{
+static void Main(string[] args)
+    {
+//Declare some variables to excercise the oprators
+  int x=10, y=5;
+  string a="abcd", b="efgh";
+
+//Todo: Basic mat oprators are +, -, /, *
+  Consle.WriteLine("----Basic Math-----");
+  Consle.WriteLine((x / y);
+  Consle.WriteLine(a+b);
+
+// Todo: Increment / decrment oprators
+    Consle.WriteLine("----Shorthand-----");
+    x++;
+    y--;
+    Consle.WriteLine(x);
+    Consle.WriteLine(y);
+// Todo: oprators can be shorthand: a = a+b
+   a += b;
+   Consle.WriteLine(a);
+
+   //Todo: Logical oprators &&,||
+   //Consle.WriteLine("---- Logic Oprators -----");
+
+   
+
+
+}
+}
+}
